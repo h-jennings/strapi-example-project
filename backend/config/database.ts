18c3e379d8c656252ca9c3b04b5d467f393a3e0f
Hunter Jennings <jenningsdhunter@gmail.com>
2022-10-05 17:00:46 -0400

    Installed pg, aws-S3 upload provider and updated the config files

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,10 +1,20 @@
-import path from 'path';
+import path from "path";
 
 export default ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "sqlite",
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(
+        __dirname,
+        "..",
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
     },
     useNullAsDefault: true,
   },
